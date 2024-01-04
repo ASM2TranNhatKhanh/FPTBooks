53ef55e53c98909625d0b9353f63b3c2ffe20ef8
Khanhtngcc19208 <91667470+TranNhatKhanh01@users.noreply.github.com>
2024-01-04 22:18:44 +0700

    Update StoreOwnerController.cs
    Edit file

--- FPTBook-main/Controllers/StoreOwnerController.cs
+++ FPTBook-main/Controllers/StoreOwnerController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using FPTBook.Models;
 using FPTBook.Areas.Identity.Data;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization; 
 
 namespace FPTBook.Controllers;
 [Authorize(Roles = "StoreOwner, Admin")]
